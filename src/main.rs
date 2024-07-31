03a4a7f1bd1473ce47738ac6fbc25a8673f226ee
Hari <211106370@qq.com>
2024-07-31 17:50:40 +0800

    feat: realize motion ctl and feedback

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,57 @@
 mod robstride;
 
-use std::time::Duration;
-use futures_util::StreamExt;
-use socketcan::{tokio::CanSocket, CanFrame, Result, EmbeddedFrame};
+use std::f32::consts::PI;
+use tokio::time::{Duration, sleep};
+use futures_util::{SinkExt, StreamExt};
+use socketcan::{tokio::CanSocket, CanFrame, Result, EmbeddedFrame, Id};
 use tokio;
-use tokio::io::AsyncReadExt;
-use tokio::time;
 use robstride::RobStrideUtils;
-use robstride::StopMode;
+// use robstride::StopMode;
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut sock_rx = CanSocket::open("can0")?;
-    let sock_tx = CanSocket::open("can0")?;
-
+    // 电机设置工具初始化
     let mut rs_util = RobStrideUtils::new(0x7f);
 
+    // CAN Socket接口初始化
+    let sock_tx = CanSocket::open("can0")?;
+    let mut sock_rx = CanSocket::open("can0")?;
+
+    // 电机初始化参数设定
     sock_tx.write_frame(rs_util.request_dev_id())?.await?;
     sock_tx.write_frame(rs_util.request_enable())?.await?;
-    sock_tx.write_frame(rs_util.request_motion(0.0, 1f32, 0f32, 0.2f32, 0.1f32))?.await?;
-    // sock_tx.write_frame(rs_util.request_stop(StopMode::Normal))?.await?;
+    sock_tx.write_frame(rs_util.write_param(0x7005, 0u32))?.await?;
+    sock_tx.write_frame(rs_util.write_param(0x7010, unsafe { std::mem::transmute(0.04f32) }))?.await?;
+    sock_tx.write_frame(rs_util.write_param(0x7011, unsafe { std::mem::transmute(0.006f32) }))?.await?;
+    sock_tx.write_frame(rs_util.write_param(0x7014, unsafe { std::mem::transmute(0.02f32) }))?.await?;
 
-    tokio::spawn( async move {
-        sock_tx.write_frame(rs_util.request_param(0x302d)).expect("Error in reading param");
-        time::sleep(Duration::from_millis(1)).await;
-    });
+    // 反馈数据储存数组
+    let mut fb_data: [f32; 4] = [0f32; 4];
 
-    while let Some(res) = sock_rx.next().await {
-        match res {
-            Ok(CanFrame::Data(frame)) => unsafe {
-                if frame.data().len() == 8 {
-                    let fdata = &frame.data()[4..8].align_to::<f32>();
-                    println!("{:?}", fdata);
+    // 电机实时线程
+    loop { // 控制频率 1000Hz
+        sock_tx.write_frame(rs_util.request_motion(0.0, 2f32, 0f32, 0.15f32, 0.2f32))?.await?;
+        match sock_rx.next().await.unwrap() {
+            Ok(CanFrame::Data(frame)) => {
+                if frame.id() == Id::from(rs_util.extended_id(2, 0x807f, 0x00)) {
+                    let mut bytes: [u8; 2] = [0; 2];
+                    bytes.copy_from_slice(&frame.data()[0..2]);
+                    fb_data[0] = u16::from_be_bytes(bytes) as f32 / 65535f32 * (8f32 * PI) - (4f32 * PI);
+                    bytes.copy_from_slice(&frame.data()[2..4]);
+                    fb_data[1] = u16::from_be_bytes(bytes) as f32 / 65535f32 * 88f32 - 44f32;
+                    bytes.copy_from_slice(&frame.data()[2..4]);
+                    fb_data[2] = u16::from_be_bytes(bytes) as f32 / 65535f32 * 34f32 - 17f32;
+                    bytes.copy_from_slice(&frame.data()[2..4]);
+                    fb_data[3] = u16::from_be_bytes(bytes) as f32 / 1000f32;
                 }
-                println!("{:?}", &frame);
             },
             Ok(CanFrame::Remote(frame)) => println!("{:?}", frame),
             Ok(CanFrame::Error(frame)) => println!("{:?}", frame),
             Err(err) => eprintln!("{}", err),
         }
+        println!("Motor status: Ang: {:<10}, Vel: {:<15}, Torque: {:<15}, Temperature: {:<15}", fb_data[0], fb_data[1], fb_data[2], fb_data[3]);
+        sleep(Duration::from_millis(1)).await;
     }
-
     Ok(())
 }
--- src/robstride.rs
+++ src/robstride.rs
@@ -1,5 +1,4 @@
 use std::f32::consts::PI;
-use std::ptr::addr_of_mut;
 use socketcan::{CanFrame, ExtendedId, EmbeddedFrame};
 
 pub enum StopMode {
@@ -17,7 +16,7 @@ impl RobStrideUtils {
         }
     }
 
-    fn set_extended_id(&mut self, com_type: u8, data_field: u16, destination: u8) -> ExtendedId {
+    pub fn extended_id(&mut self, com_type: u8, data_field: u16, destination: u8) -> ExtendedId {
         ExtendedId::new(
             (com_type as u32) << 24 | (data_field as u32) << 8 | (destination as u32)
         ).unwrap()
@@ -25,19 +24,19 @@ impl RobStrideUtils {
 
     pub fn request_dev_id(&mut self) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(0, 0x0000, self.id), &[0]
+            self.extended_id(0, 0x0000, self.id), &[0]
         ).unwrap()
     }
 
     pub fn request_motion(&mut self, torque: f32, angle: f32, velocity: f32, kp: f32, kd: f32) -> CanFrame {
-        let torqueu16 = ((torque + 17f32 / 34f32) * 65535f32) as u16;
-        let angleu16 = ((angle + 4f32*PI / 8f32*PI) * 65535f32) as u16;
-        let velocityu16 = ((velocity + 44f32 / 88f32) * 65535f32) as u16;
+        let torqueu16 = (((torque + 17f32) / 34f32) * 65535f32) as u16;
+        let angleu16 = ((angle + 4f32*PI) / (8f32*PI) * 65535f32) as u16;
+        let velocityu16 = (((velocity + 44f32) / 88f32) * 65535f32) as u16;
         let kpu16 = ((kp / 500f32) * 65535f32) as u16;
         let kdu16 = ((kd / 5f32) * 65535f32) as u16;
 
         CanFrame::new(
-            self.set_extended_id(1, torqueu16, self.id), &[
+            self.extended_id(1, torqueu16, self.id), &[
                 (angleu16 >> 8) as u8,
                 angleu16 as u8,
                 (velocityu16 >> 8) as u8,
@@ -52,31 +51,31 @@ impl RobStrideUtils {
 
     pub fn request_enable(&mut self) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(3, 0x0000, self.id), &[0]
+            self.extended_id(3, 0x0000, self.id), &[0]
         ).unwrap()
     }
 
     pub fn request_stop(&mut self, mode: StopMode) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(4, 0x0000, self.id), &[mode as u8]
+            self.extended_id(4, 0x0000, self.id), &[mode as u8]
         ).unwrap()
     }
 
     pub fn set_zero_point(&mut self) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(6, 0x0000, self.id), &[1]
+            self.extended_id(6, 0x0000, self.id), &[1]
         ).unwrap()
     }
 
     pub fn set_dev_id(&mut self, request_id: u8) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(7, (request_id as u16) << 8, self.id), &[1]
+            self.extended_id(7, (request_id as u16) << 8, self.id), &[1]
         ).unwrap()
     }
 
     pub fn request_param(&mut self, index: u16) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(17, 0x0000, self.id),
+            self.extended_id(17, 0x0000, self.id),
             &[
                 index as u8,
                 (index >> 8) as u8
@@ -86,7 +85,7 @@ impl RobStrideUtils {
 
     pub fn write_param(&mut self, index: u16, param: u32) -> CanFrame {
         CanFrame::new(
-            self.set_extended_id(18, 0x0000, self.id),
+            self.extended_id(18, 0x0000, self.id),
             &[
                 index as u8,
                 (index >> 8) as u8,
